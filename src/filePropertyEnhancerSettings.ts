afe2ee7d19e8dff3dde720eb4722f396ebb2d4c3
Quorafind <bonopengate@gmail.com>
2023-10-12 15:24:22 +0800

    chore: update file for review

--- src/filePropertyEnhancerSettings.ts
+++ src/filePropertyEnhancerSettings.ts
@@ -1,57 +1,9 @@
 import type { MetadataIcon } from "./types/global";
 
-type StyleOption = "solid" | "gradient";
-export type BannerDragModOption = "none" | "shift" | "ctrl" | "alt" | "meta";
-type IconHorizontalOption = "left" | "center" | "right" | "custom";
-type IconVerticalOption = "above" | "center" | "below" | "custom";
-type TitlePlacementOption = "below-icon" | "next-to-icon";
-
 export interface filePropertyEnhancerSettings {
 	iconList: MetadataIcon[];
-	banners: {
-		height: number | null;
-		style: StyleOption;
-		holdingDragModKey: boolean;
-		frontmatterField: string;
-		bannerDragModifier: BannerDragModOption;
-		iconHorizontalAlignment: IconHorizontalOption;
-		iconHorizontalTransform: string | null;
-		iconVerticalAlignment: IconVerticalOption;
-		iconVerticalTransform: string | null;
-		useTwemoji: boolean;
-		showInInternalEmbed: boolean;
-		showInPreviewEmbed: boolean;
-		internalEmbedHeight: number | null;
-		previewEmbedHeight: number | null;
-		showPreviewInLocalModal: boolean;
-		localSuggestionsLimit: number | null;
-		bannersFolder: string | null;
-		allowMobileDrag: boolean;
-		titlePlacement: TitlePlacementOption;
-	}
 }
 
 export const DEFAULT_SETTINGS: filePropertyEnhancerSettings = {
 	iconList: [],
-	banners: {
-		height: 320,
-		bannerDragModifier: "alt",
-		holdingDragModKey: false,
-		style: "solid",
-		showInInternalEmbed: true,
-		showInPreviewEmbed: true,
-		iconHorizontalAlignment: "left",
-		iconVerticalAlignment: "center",
-		useTwemoji: false,
-		showPreviewInLocalModal: true,
-		allowMobileDrag: false,
-		titlePlacement: "below-icon",
-		internalEmbedHeight: 200,
-		previewEmbedHeight: 120,
-		frontmatterField: "banner",
-		iconHorizontalTransform: "0px",
-		iconVerticalTransform: "0px",
-		localSuggestionsLimit: 10,
-		bannersFolder: "/",
-	}
 };

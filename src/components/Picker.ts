78bfd21cfb8d79c1cda1bed5933de729d7418675
Quorafind <bonopengate@gmail.com>
2024-02-02 18:00:31 +0800

    feat: remove icon

--- src/components/Picker.ts
+++ src/components/Picker.ts
@@ -1,17 +1,30 @@
-import { App, Modal } from "obsidian";
+import { App, ExtraButtonComponent, Modal } from "obsidian";
 import PickerComponent from './UI/PickerComponent.svelte';
+import type FilePropertyEnhancerPlugin from "../filePropertyEnhancerIndex";
+import { removeIcon } from "../utils/utils";
 
 export class PickerModal extends Modal {
 	private cb: (emoji: any) => void;
 	private picker: PickerComponent;
+	private plugin: FilePropertyEnhancerPlugin;
+	private property: any;
 
-	constructor(app: App, cb: (emoji: any) => void) {
+	constructor(plugin: FilePropertyEnhancerPlugin, app: App, property: any, cb: (emoji: any) => void) {
 		super(app);
 		this.cb = cb;
+		this.plugin = plugin;
+		this.property = property;
 	}
 
 	onOpen() {
 		this.containerEl.toggleClass("icon-picker-modal", true);
+		const buttonEl = createEl('div', {cls: 'icon-delete-btn'});
+		// Insert the button into the modal the first child of the contentEl
+		new ExtraButtonComponent(buttonEl).setIcon('trash').onClick(async () => {
+			await removeIcon(this.plugin, this.property);
+			this.close();
+		});
+		this.modalEl.insertBefore(buttonEl, this.modalEl.firstChild);
 		this.picker = new PickerComponent({
 			target: (this as any).contentEl, props: {}
 		});
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -6,16 +6,32 @@ import type { IconType, MetadataIcon } from "../types/global";
 type IconRenderType = "file-property" | "all-properties";
 
 export const createModal = (context: FilePropertyEnhancerPlugin, property: any) => {
-	return new PickerModal(context.app, async (selected) => {
+	return new PickerModal(context, context.app, property, async (selected) => {
+		console.log(property);
 		const icon = {
 			name: property?.entry?.key || property?.property.key,
 			icon: selected.emoji ? selected.emoji.native : selected.icon,
 			type: selected.type as IconType,
-		}
+		};
 		await saveIcon(context, icon);
+		setPropertyIcon(property, icon, "file-property");
 		context.app.metadataTypeManager.trigger("changed", property?.entry?.key || property?.property.key);
 	});
-}
+};
+
+export const removeIcon = async (context: FilePropertyEnhancerPlugin, property: any) => {
+	const icon = getIcon(context, property?.entry?.key || property?.property.key);
+	if (icon) {
+		const index = context.settings.iconList.findIndex((item) => item.name === icon.name);
+		if (index !== -1) {
+			context.settings.iconList.splice(index, 1);
+			await context.saveSettings();
+		}
+	}
+	console.log(property);
+	property.iconEl.empty();
+	property.renderProperty(property.entry, true);
+};
 
 export const setPropertyIcon = (property: any, icon: MetadataIcon, renderedType: IconRenderType) => {
 	switch (icon.type) {
@@ -24,7 +40,7 @@ export const setPropertyIcon = (property: any, icon: MetadataIcon, renderedType:
 		case "lucide":
 			return renderLucideIcon(property, icon, renderedType);
 	}
-}
+};
 
 export const saveIcon = async (context: FilePropertyEnhancerPlugin, icon: MetadataIcon) => {
 	const index = context.settings.iconList.findIndex((item) => item.name === icon.name);
@@ -34,7 +50,7 @@ export const saveIcon = async (context: FilePropertyEnhancerPlugin, icon: Metada
 		context.settings.iconList.push(icon);
 	}
 	await context.saveSettings();
-}
+};
 
 export const getIcon = (context: FilePropertyEnhancerPlugin, key: string): MetadataIcon | null => {
 	const index = context.settings.iconList.findIndex((item) => item.name === key);
@@ -43,20 +59,20 @@ export const getIcon = (context: FilePropertyEnhancerPlugin, key: string): Metad
 	} else {
 		return null;
 	}
-}
+};
 
 export const renderEmojiIcon = (property: any, icon: MetadataIcon, type: IconRenderType) => {
 	const button = createIconBase(property, type);
 	button.extraSettingsEl.empty();
-	button.extraSettingsEl.createSpan({text: icon.icon})
+	button.extraSettingsEl.createSpan({text: icon.icon});
 	return button;
-}
+};
 
 export const renderLucideIcon = (property: any, icon: MetadataIcon, type: IconRenderType) => {
 	const button = createIconBase(property, type);
 	button.setIcon(icon.icon);
 	return button;
-}
+};
 
 export const createIconBase = (property: any, type: IconRenderType) => {
 	property.iconEl.empty();
@@ -67,4 +83,4 @@ export const createIconBase = (property: any, type: IconRenderType) => {
 	button.extraSettingsEl.toggleClass(["metadata-style-icon"], true);
 
 	return button;
-}
+};
